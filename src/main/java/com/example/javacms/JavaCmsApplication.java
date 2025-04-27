c2727b629c920aa75cc9bd6dee543051e2a9fc04
m.bagheri <bagheri.almas@gmail.com>
2025-04-27 08:25:24 +0330

    first commit

---
+++ src/main/java/com/example/javacms/JavaCmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacms/JavaCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
