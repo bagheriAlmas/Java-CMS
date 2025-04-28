2a82b721dc4539f760d451abd6a90dad38586604
m.bagheri <bagheri.almas@gmail.com>
2025-04-28 14:04:38 +0330

    Add methods to do CRUD for category

--- src/main/java/com/example/javacms/controller/CategoryController.java
+++ src/main/java/com/example/javacms/controller/CategoryController.java
@@ -1,13 +1,41 @@
 package com.example.javacms.controller;
 
+import com.example.javacms.entity.Category;
 import com.example.javacms.service.CategoryService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/category")
-@RequiredArgsConstructor
 public class CategoryController {
-    private final CategoryService categoryService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    List<Category> getAllCategories(){
+        return categoryService.findAll();
+    }
+
+    @GetMapping("{id}")
+    Category getCategoryById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+
+    @PostMapping
+    private void insertCategory(@RequestBody Category category) {
+        categoryService.save(category);
+    }
+
+    @PutMapping("{id}")
+    private void updateCategory(@PathVariable("id") Long id, @RequestBody Category category) {
+        categoryService.update(category);
+    }
+
+    @DeleteMapping("{id}")
+    private void updateCategory(@PathVariable("id") Long id) {
+        categoryService.deleteById(id);
+    }
 }
