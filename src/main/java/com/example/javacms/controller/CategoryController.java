8e26b41544f0c330f765ec32e49ea255de204971
mahdibagheri <bagheri.almas@gmail.com>
2025-04-29 22:50:18 +0330

    Add methods to Create, Update, Delete, FindAll and FindById

--- src/main/java/com/example/javacms/controller/CategoryController.java
+++ src/main/java/com/example/javacms/controller/CategoryController.java
@@ -2,40 +2,39 @@ package com.example.javacms.controller;
 
 import com.example.javacms.entity.Category;
 import com.example.javacms.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/category")
+@RequiredArgsConstructor
 public class CategoryController {
-
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping
-    List<Category> getAllCategories(){
+    List<Category> getAllCategories() {
         return categoryService.findAll();
     }
 
     @GetMapping("{id}")
-    Category getCategoryById(@PathVariable Long id){
+    Category getCategoryById(@PathVariable Long id) {
         return categoryService.findById(id);
     }
 
     @PostMapping
-    private void insertCategory(@RequestBody Category category) {
+    void insertCategory(@RequestBody Category category) {
         categoryService.save(category);
     }
 
     @PutMapping("{id}")
-    private void updateCategory(@PathVariable("id") Long id, @RequestBody Category category) {
-        categoryService.update(category);
+    void updateCategory(@PathVariable("id") Long id, @RequestBody Category category) {
+        categoryService.update(id, category);
     }
 
     @DeleteMapping("{id}")
-    private void updateCategory(@PathVariable("id") Long id) {
+    void updateCategory(@PathVariable("id") Long id) {
         categoryService.deleteById(id);
     }
 }
