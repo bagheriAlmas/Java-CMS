66c409a0b4317f2b001b74970e8430bcbc0ea300
mahdibagheri <bagheri.almas@gmail.com>
2025-04-29 22:39:45 +0330

    Add basic CRUD for category

--- src/main/java/com/example/javacms/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/javacms/service/impl/CategoryServiceImpl.java
@@ -1,36 +1,39 @@
 package com.example.javacms.service.impl;
 
 import com.example.javacms.entity.Category;
+import com.example.javacms.repository.CategoryRepository;
 import com.example.javacms.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
-
+    private final CategoryRepository categoryRepository;
     @Override
     public List<Category> findAll() {
-        return List.of();
+        return categoryRepository.findAll();
     }
 
     @Override
     public Category findById(long id) {
-        return null;
+        return categoryRepository.findById(id).orElse(null);
     }
 
     @Override
     public void save(Category category) {
-
+        categoryRepository.save(category);
     }
 
     @Override
     public void update(Category category) {
-
+        categoryRepository.save(category);
     }
 
     @Override
     public void deleteById(long id) {
-
+        categoryRepository.deleteById(id);
     }
 }
