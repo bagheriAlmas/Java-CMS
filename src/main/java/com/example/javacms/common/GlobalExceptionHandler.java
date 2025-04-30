c6e3f666d673b3e53d4760caafc4c33640d2800a
m.bagheri <bagheri.almas@gmail.com>
2025-04-30 12:52:47 +0330

    Handle RoleNotFoundException in GlobalExceptionHandler

--- src/main/java/com/example/javacms/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/javacms/common/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.javacms.common;
 
 import com.example.javacms.common.exceptions.CategoryNotFoundException;
+import com.example.javacms.common.exceptions.RoleNotFoundException;
 import com.example.javacms.common.exceptions.TagNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
@@ -33,6 +34,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TagNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleTagNotFoundException(TagNotFoundException exception) {
+        final var response = createErrorMessage(exception.getMessage(), exception.getStatus());
+        return new ResponseEntity<>(response, exception.getStatus());
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleRoleNotFoundException(RoleNotFoundException exception) {
         final var response = createErrorMessage(exception.getMessage(), exception.getStatus());
         return new ResponseEntity<>(response, exception.getStatus());
     }
