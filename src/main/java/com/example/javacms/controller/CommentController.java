002c3c2e4f8f836c7ae1316451f97a8ee75b11fe
m.bagheri <bagheri.almas@gmail.com>
2025-04-30 14:23:38 +0330

    Use CRUD methods fo comment in controller

--- src/main/java/com/example/javacms/controller/CommentController.java
+++ src/main/java/com/example/javacms/controller/CommentController.java
@@ -1,14 +1,48 @@
 package com.example.javacms.controller;
 
+import com.example.javacms.entity.dto.CommentRequestDto;
+import com.example.javacms.entity.dto.CommentResponseDto;
 import com.example.javacms.service.CommentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/comment")
+@RequiredArgsConstructor
 public class CommentController {
 
-    @Autowired
-    private  CommentService commentService;
+    private final CommentService commentService;
+
+    @GetMapping
+    ResponseEntity<List<CommentResponseDto>> getAllComments() {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.findAll());
+    }
+
+    @GetMapping("{id}")
+    ResponseEntity<CommentResponseDto> getCommentById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.findById(id));
+    }
+
+    @PostMapping
+    ResponseEntity<Void> insertComment(@RequestBody @Valid CommentRequestDto requestDto) {
+        commentService.save(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("{id}")
+    ResponseEntity<Void> updateComment(@PathVariable("id") Long id, @RequestBody @Valid CommentRequestDto requestDto) {
+        commentService.update(id, requestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("{id}")
+    ResponseEntity<Void> deleteComment(@PathVariable("id") Long id) {
+        commentService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
