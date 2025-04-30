a004b8c9cee21f62a1134c2b795092ae0bcb4765
m.bagheri <bagheri.almas@gmail.com>
2025-04-30 17:27:09 +0330

    Add mapper to ResponseDtos

--- src/main/java/com/example/javacms/entity/dto/CategoryResponseDto.java
+++ src/main/java/com/example/javacms/entity/dto/CategoryResponseDto.java
@@ -1,8 +1,16 @@
 package com.example.javacms.entity.dto;
 
+import com.example.javacms.entity.Category;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CategoryResponseDto(
         @JsonProperty("id") Long id,
         @JsonProperty("name") String name) {
+
+    public static Category toEntity(CategoryResponseDto dto) {
+        Category category = new Category();
+        category.setId(dto.id);
+        category.setName(dto.name);
+        return category;
+    }
 }
--- src/main/java/com/example/javacms/entity/dto/TagResponseDto.java
+++ src/main/java/com/example/javacms/entity/dto/TagResponseDto.java
@@ -1,8 +1,16 @@
 package com.example.javacms.entity.dto;
 
+import com.example.javacms.entity.Tag;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TagResponseDto(
         @JsonProperty("id") Long id,
         @JsonProperty("name") String name) {
+
+    public static Tag toEntity(TagResponseDto tagResponseDto) {
+        Tag tag = new Tag();
+        tag.setId(tagResponseDto.id);
+        tag.setName(tagResponseDto.name);
+        return tag;
+    }
 }
