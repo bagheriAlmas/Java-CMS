83e628e76c7ae3c7876704d39f9d6cc6233688ad
m.bagheri <bagheri.almas@gmail.com>
2025-04-30 14:51:13 +0330

    Fix wrong naming

--- src/main/java/com/example/javacms/entity/dto/CommentRequestDto.java
+++ src/main/java/com/example/javacms/entity/dto/CommentRequestDto.java
@@ -6,19 +6,19 @@ import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 public record CommentRequestDto(
-        @JsonProperty("name")
+        @JsonProperty("content")
         @NotBlank(message = "CommentRequestDto.content.notBlank")
         @NotEmpty(message = "CommentRequestDto.content.notEmpty")
         @Length(max = 100, message = "CommentRequestDto.content.maxLength")
         String content,
 
-        @JsonProperty("name")
+        @JsonProperty("articleId")
         @NotBlank(message = "CommentRequestDto.article.notBlank")
         @NotEmpty(message = "CommentRequestDto.article.notEmpty")
         @Length(max = 100, message = "CommentRequestDto.article.maxLength")
         long articleId,
 
-        @JsonProperty("name")
+        @JsonProperty("memberId")
         @NotBlank(message = "CommentRequestDto.member.notBlank")
         @NotEmpty(message = "CommentRequestDto.member.notEmpty")
         @Length(max = 100, message = "CommentRequestDto.member.maxLength")
--- src/main/java/com/example/javacms/entity/dto/RoleRequestDto.java
+++ src/main/java/com/example/javacms/entity/dto/RoleRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 public record RoleRequestDto(
-        @JsonProperty("name")
+        @JsonProperty("roleName")
         @NotBlank(message = "RoleRequestDto.name.notBlank")
         @NotEmpty(message = "RoleRequestDto.name.notEmpty")
         @Length(max = 100, message = "RoleRequestDto.name.maxLength") String roleName) {
